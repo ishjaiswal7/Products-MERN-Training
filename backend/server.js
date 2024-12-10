b99586c032761386da23ac67108693bc5596389a
Ish Jaiswal <ishjaiswal7@gmail.com>
2024-12-11 01:19:55 +0530

    Delete Product Functionality Added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'; 
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
@@ -24,6 +25,17 @@ app.post('/api/products', async (req, res) => {
         res.status(500).json({success:false, message: 'Server Error'});
     }
 });
+
+app.delete("/api/products/:id", async (req, res) => {
+    const{id} = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: 'Product deleted successfully'});
+    }catch (error) {
+        console.error("Error in deleting product", error.message);
+        res.status(500).json({success:false, message: 'Server Error'});
+    }
+})
     
 app.listen(5000, () => {
     connectDB();
