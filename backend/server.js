9564122701116a9595d9f219986f8f28db284fdb
Ish Jaiswal <ishjaiswal7@gmail.com>
2024-12-12 22:12:37 +0530

    Get and Update Product featues added

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,23 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose, { mongo } from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success: true, data: products});
+    } catch (error) {
+        console.error("Error in fetching products", error.message);
+        res.status(500).json({success: false, message: 'Server Error'});
+    }
+});
 
 app.post('/api/products', async (req, res) => {
     const product = req.body; // user will send this data
@@ -26,6 +37,23 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
+app.put("/api/products/:id", async (req, res) => {  
+    const {id} = req.params;
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: 'No product with that id'});
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+        res.status(200).json({success: true, data: updatedProduct});
+    } catch (error) {
+        console.error("Error in updating product", error.message);
+        res.status(404).json({success: false, message: 'Product not found'});
+    }
+});
+
 app.delete("/api/products/:id", async (req, res) => {
     const{id} = req.params;
     try {
@@ -33,7 +61,7 @@ app.delete("/api/products/:id", async (req, res) => {
         res.status(200).json({success: true, message: 'Product deleted successfully'});
     }catch (error) {
         console.error("Error in deleting product", error.message);
-        res.status(500).json({success:false, message: 'Server Error'});
+        res.status(404).json({success: false, message: 'Product not found'});
     }
 })
     
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 This is a small project based MERN Stack
 
 npm init -y
+
 npm install express mongoose dotenv
+
+npm run dev
